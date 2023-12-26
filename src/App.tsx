4e391c2d8733859f1bd4d00e449e3bc7bf802f84
Tanya627 <tanyaocampo96@yahoo.com>
2023-12-26 14:59:55 -0600

    Icon on click functionality, structured Instructions component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import "./App.css";
+import { useState } from "react";
+import Instructions from "./gameContent/Instructions";
 
 function App() {
-  return (
-    <>
-      <div>
+  const [needHelp, setState] = useState<boolean>(false);
+
+  if (needHelp) {
+    return <Instructions setState={setState}></Instructions>
+  } else {
+    return (
+      <>
         <div>
-          <h1 className="title">A Classic Game of Hangman</h1>
-        </div>
-        <div>
-          <h3>Select a Category</h3>
-          <div className="category-buttons">
-            <button>World Countries 🌎</button>
-            <button>Animals 🐕</button>
-            <button>USA Capitals 🇺🇸</button>
+          <div>
+            <h1 className="title">A Classic Game of Hangman</h1>
           </div>
-          <div id="question-icon">
-            <span className="material-symbols-outlined">help</span>
+          <div>
+            <h3>Select a Category</h3>
+            <div className="category-buttons">
+              <button>World Countries 🌎</button>
+              <button>Animals 🐕</button>
+              <button>USA Capitals 🇺🇸</button>
+            </div>
+            <div id="question-icon">
+              <a onClick={() => setState(true)}>
+                <span className="material-symbols-outlined">help</span>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
