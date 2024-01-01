ddb6b605c94db5ff8875701df4c8adad5da7e775
Tanya627 <tanyaocampo96@yahoo.com>
2024-01-01 16:20:50 -0600

    added answer reveal on game completion

--- src/gameContent/PopUp.tsx
+++ src/gameContent/PopUp.tsx
@@ -5,18 +5,22 @@ type PopUpProperties = {
   setGameState: React.Dispatch<React.SetStateAction<boolean>>;
   setEndGamePopUp: React.Dispatch<React.SetStateAction<boolean>>;
   resetGame: () => void;
+  answer: string;
 };
 const PopUp = ({
   title,
   setGameState,
   setEndGamePopUp,
   resetGame,
+  answer,
 }: PopUpProperties) => {
   return (
     <div>
       <div className="popup-square">
         <div className="popup-title">
           <h1 className="title">{title}</h1>
+          <h3 className="answer">The answer was:</h3>
+          <h1 className="answer">{answer}</h1>
         </div>
         <div className="play-again-button category-text ">
           <button
